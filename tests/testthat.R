914293d781bd0c7bb5a41e8fca3642cd18e42216
Benno Pütz <puetz@psych.mpg.de>
2021-07-07 10:19:34 +0200

    vcr

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,15 +7,22 @@ test_check("applepie")
 #   pkgs <- get_packages("maelle")
 #   expect_vector(pkgs, character())
 #})
+test_that("get_packages() works", {
+  vcr::use_cassette("packages", {
+    pkgs <- get_packages("maelle")
+  })
+  expect_vector(pkgs, character())
+})
 
 test_that("maintainers OK", {
   mtns <- get_maintainers('ggseg')
   expect_equal(dim(mtns), 4)
 })
 
-test_that("get_packages() works", {
-  vcr::use_cassette("packages", {
-    pkgs <- get_packages("maelle")
+
+test_that("get_packages() error", {
+  vcr::skip_if_vcr_off()
+  vcr::use_cassette("packages-error", {
+    expect_error(get_packages("maelle"), "take off")
   })
-  expect_vector(pkgs, character())
 })

726afa0c3cb3212f522d86f14a7c263b1e06e520
Benno Pütz <puetz@psych.mpg.de>
2021-07-07 12:02:19 +0200

    setup

--- tests/setup-applepie.R
+++ tests/setup-applepie.R
@@ -1,4 +1,5 @@
 library("vcr") # *Required* as vcr is set up on loading
+
 invisible(vcr::vcr_configure(
   dir = vcr::vcr_test_path("fixtures"),
   filter_request_headers = list(Authorization = "not-my-secret-hehe")

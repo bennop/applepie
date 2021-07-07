af5da9df6679ba03f32b6b9917a902ce08df25a4
Benno Pütz <puetz@psych.mpg.de>
2021-07-07 10:59:59 +0200

    Auth example

---
+++ R/setup-applepie.R
@@ -0,0 +1,10 @@
+library("vcr") # *Required* as vcr is set up on loading
+invisible(vcr::vcr_configure(
+  dir = vcr::vcr_test_path("fixtures"),
+  filter_request_headers = list(Authorization = "not-my-secret-hehe")
+))
+
+vcr::check_cassette_names()
+if (!nzchar(Sys.getenv("SECRET_PLANET_TOKEN"))) {
+  Sys.setenv("SECRET_PLANET_TOKEN" = "abcd123")
+}
